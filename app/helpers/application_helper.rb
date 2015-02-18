0fd56d80e8f7b595d38ce97b44813550edb2f88a
Tristan Dunn <tristanzdunn@gmail.com>
2015-02-17 22:06:55 -0600

    Display a random tip in the initial player UI.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 module ApplicationHelper
+  TIPS = [
+   "Use the progress bar to seek.",
+   "Use <kbd>M</kbd> to mute or unmute.",
+   "Use <kbd>space</kbd> to pause and play.",
+   "Use <kbd>up</kbd> and <kbd>down</kbd> to adjust the volume."
+  ].freeze
+
   def csrf_meta_tags
     [
       tag("meta", name: "csrf-param", content: request_forgery_protection_token),
@@ -8,5 +15,9 @@ module ApplicationHelper
 
   def tag(name, options = nil, open = false, escape = true)
     "<#{name}#{tag_options(options, escape)}>".html_safe
+  end
+
+  def tip
+    TIPS.sample.html_safe
   end
 end
